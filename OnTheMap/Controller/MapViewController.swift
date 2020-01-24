004711de1ed3d112fc14196776ee6c2693624c72
xthian82 <crecalde@gmail.com>
2020-01-24 01:22:34 -0300

    map added

---
+++ OnTheMap/Controller/MapViewController.swift
@@ -0,0 +1,128 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Cristhian Recalde on 1/24/20.
+//  Copyright © 2020 Cristhian Recalde. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    //MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+    var annotations = [MKPointAnnotation]()
+    
+    //MARK: View Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let locations = hardCodedLocationData()
+        
+        for dict in locations {
+            annotations.append(getAnnotationFromDictionary(dict))
+        }
+        
+        self.mapView.addAnnotations(annotations)
+    }
+    
+    //MARK: Map Actions
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
+    }
+
+    // MARK: - Sample Data
+    
+    func getAnnotationFromDictionary(_ dictionary: [String: Any]) -> MKPointAnnotation {
+        // Notice that the float values are being used to create CLLocationDegree values.
+        // This is a version of the Double type.
+        let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+        let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+        
+        // The lat and long are used to create a CLLocationCoordinates2D instance.
+        let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        let first = dictionary["firstName"] as! String
+        let last = dictionary["lastName"] as! String
+        let mediaURL = dictionary["mediaURL"] as! String
+        
+        // Here we create the annotation and set its coordiate, title, and subtitle properties
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "\(first) \(last)"
+        annotation.subtitle = mediaURL
+        
+        return annotation
+    }
+    
+    // Some sample data. This is a dictionary that is more or less similar to the
+    // JSON data that you will download from Parse.
+    
+    func hardCodedLocationData() -> [[String : Any]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Jason",
+                "lastName" : "Schatz",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 2362758535,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
+    }
+}
+
