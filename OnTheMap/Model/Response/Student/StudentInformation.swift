35263668115854c88596050a59ddea07248cfa08
xthian82 <crecalde@gmail.com>
2020-01-27 22:39:52 -0300

    missing files

---
+++ OnTheMap/Model/Response/Student/StudentInformation.swift
@@ -0,0 +1,35 @@
+//
+//  StudentInformation.swift
+//  OnTheMap
+//
+//  Created by Cristhian Recalde on 1/27/20.
+//  Copyright © 2020 Cristhian Recalde. All rights reserved.
+//
+
+import Foundation
+
+struct StudentInformation: Codable {
+    let createdAt: String
+    let firstName: String
+    let lastName: String
+    let latitude: Double
+    let longitude: Double
+    let mapString: String
+    let mediaURL: String
+    let objectId: String
+    let uniqueKey: String
+    let updatedAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case createdAt
+        case firstName
+        case lastName
+        case latitude
+        case longitude
+        case mapString
+        case mediaURL
+        case objectId
+        case uniqueKey
+        case updatedAt
+    }
+}
---
+++ OnTheMap/Model/Response/Student/StudentLocations.swift
@@ -0,0 +1,18 @@
+//
+//  StudentLocations.swift
+//  OnTheMap
+//
+//  Created by Cristhian Recalde on 1/27/20.
+//  Copyright © 2020 Cristhian Recalde. All rights reserved.
+//
+
+import Foundation
+
+struct StudentLocations: Codable {
+
+    let results: [StudentInformation]?
+    
+    enum CodingKeys: String, CodingKey {
+        case results
+    }
+}
